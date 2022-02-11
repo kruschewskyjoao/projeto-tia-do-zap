762c685e1e94ba87f1242edc18143c670b5728d4
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-02-11 17:41:01 -0300

    criando backup dos arquivos html, css e js

---
+++ bkp/script.js
@@ -0,0 +1,98 @@
+const gettingData = async () => {
+    const url = `https://api.thecatapi.com/v1/images/search?limit=6&page=10&order=Desc`;
+    const response = await fetch (url);
+    const result = await response.json();
+    return result;
+}
+
+const createKitty = async (elements) => {
+    const ol = document.querySelector('#galeria');
+        elements.forEach((element) => {
+        const cats= document.createElement('img');
+        cats.src = element.url;
+        cats.className = 'cats-image';
+        ol.appendChild(cats);
+    })
+}
+
+const createClickImage = async () => {
+    const gatos = document.querySelectorAll('.cats-image');
+    gatos.forEach((cat) => {
+        cat.addEventListener('click', () => {
+            const div = document.querySelector('.image-container');
+            const newCat = document.createElement('img');
+
+            newCat.src = cat.src;
+            newCat.className = 'cat-message';
+            
+            div.innerHTML = '';
+            div.appendChild(newCat);
+        })
+    });
+}
+
+
+
+const ol = document.querySelector('#galeria');
+const botao = document.querySelector('.new-cats');
+const img = document.querySelectorAll('.cats-image');
+    
+botao.addEventListener('click', async () => {
+    const func = await gettingData();
+
+    ol.innerHTML = '';
+    
+    await createKitty(func);  
+    await createClickImage();
+});
+
+const createText = () => {
+    const inputText = document.querySelector('.input-text');
+    const imageConteiner = document.querySelector('.image-container');
+    const firstParagraph = document.createElement('p');
+    firstParagraph.className = 'textImage'
+    firstParagraph.innerText = inputText.value;
+    imageConteiner.appendChild(firstParagraph);
+    inputText.value = '';
+}
+
+
+const buttonUp = document.querySelector('.button-up');
+buttonUp.addEventListener('click', createText);
+
+const buttonDown = document.querySelector('.button-down');
+buttonDown.addEventListener('click', createText);
+
+const buttonCenter = document.querySelector('.button-center');
+buttonCenter.addEventListener('click', createText);
+
+const cleanText = document.querySelector('.clean')
+cleanText.addEventListener('click', () => {
+    const paragraphs = document.querySelectorAll('.textImage');
+    paragraphs.forEach((paragraph) => {
+        paragraph.innerHTML = '';
+    });
+});
+
+
+// console.log(gettingCat());
+// console.log(gettingCat())
+/* function fetchPics() {
+    fetch('https://api.thecatapi.com/v1/images/search')
+    .then(response => response.json())
+    .then((data) =>{
+        console.log(data)
+    })
+} */
+
+/* const createElements = () => {
+    const ol = document.getElementById('galeria'); */
+
+window.onload = async () => {
+   const gData = await gettingData();
+   const gKitty = await createKitty(gData);
+
+   createClickImage();
+}
+ 
+  // --header 'x-api-key: DEMO-API-KEY'
