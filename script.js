5fb4b2e23829bba0cc77b4ed01dff863cec85908
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-02-11 12:01:23 -0300

    Criaçao de novas funçoes Js

--- script.js
+++ script.js
@@ -9,12 +9,41 @@ const createKitty = async (elements) => {
     const ol = document.querySelector('#galeria');
         elements.forEach((element) => {
         const cats= document.createElement('img');
-        cats.setAttribute('src',element.url)
+        cats.src = element.url;
+        cats.className = 'cats-image';
         ol.appendChild(cats);
     })
 }
 
+const createClickImage = async () => {
+    const gatos = document.querySelectorAll('.cats-image');
+    gatos.forEach((cat) => {
+        cat.addEventListener('click', () => {
+            const div = document.querySelector('.image-container');
+            const newCat = document.createElement('img');
+
+            newCat.src = cat.src;
+            
+            div.innerHTML = '';
+            div.appendChild(newCat);
+        })
+    });
+}
+
+
+const randomCats = async () => {
+    const ol = document.querySelector('#galeria');
+    const botao = document.querySelector('.new-cats');
+    const func = await gettingData();
+    const img = document.querySelectorAll('.cats-image');
+
+    botao.addEventListener('click', () => {
+
+        ol.innerHTML = '';
     
+        createKitty(func);  
+    });
+}
 
 // console.log(gettingCat());
 // console.log(gettingCat())
@@ -29,10 +58,13 @@ const createKitty = async (elements) => {
 /* const createElements = () => {
     const ol = document.getElementById('galeria'); */
 
+    randomCats();
 
 window.onload = async () => {
    const gData = await gettingData();
    const gKitty = await createKitty(gData);
+
+   createClickImage();
 }
  
   // --header 'x-api-key: DEMO-API-KEY'
